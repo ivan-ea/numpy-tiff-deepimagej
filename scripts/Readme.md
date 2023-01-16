95907294ca46bec6f2388df56787bc8054798c47
ESTEVEZ ALBUJA <hestevez@pa.uc3m.es>
2023-01-16 15:50:39 +0100

    convert from numpy to tiff with bioimageio.core function

---
+++ scripts/Readme.md
@@ -0,0 +1,9 @@
+- bioimageio.core installed as dev
+- change `__init__.py` to look like:
+
+````python
+from .add_weights import add_weights
+from .build_model import build_model, _write_sample_data
+````
+
+absolute path: `C:\Users\hestevez\.conda\envs\bio-conv-weights\Lib\site-packages\bioimageio\core\build_spec\__init__.py`
--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,27 @@
 
 import constants_and_functions as cf
 from bioimageio.core import load_resource_description
+from bioimageio.core.build_spec import _write_sample_data
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 a_model = load_resource_description(cf.path_model_2)
+
+
+input_im = np.load(a_model.test_inputs[0])
+
+pixel_sizes = np.zeros_like(input.shape)
+
+in_axes = ["".join(a.axes) for a in a_model.inputs]
+out_axes = ["".join(a.axes) for a in a_model.outputs]
+
+_write_sample_data(input_paths=a_model.test_inputs,
+                   output_paths=a_model.test_outputs,
+                   input_axes=in_axes,
+                   output_axes=out_axes,
+                   export_folder=cf.Path("."),
+                   pixel_sizes=None)
 
 
 def print_hi(name):
@@ -14,3 +33,4 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('World')
 
+#_write_sample_data()
